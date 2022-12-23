78fbac74a8543c2b72c9acb275b43b6477737aa2
Ilya Tikhomirov <76myxomor76@gmail.com>
2022-12-24 01:45:40 +0300

    Add create method in MenuController

--- src/main/java/ru/iooko/votingapp/controllers/menu/MenuController.java
+++ src/main/java/ru/iooko/votingapp/controllers/menu/MenuController.java
@@ -1,12 +1,16 @@
 package ru.iooko.votingapp.controllers.menu;
 
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+import ru.iooko.votingapp.dto.MenuDTO;
 import ru.iooko.votingapp.model.Menu;
+import ru.iooko.votingapp.util.accessory.MenuUtil;
+
+import javax.validation.Valid;
+import java.net.URI;
 
 @RestController
 @Secured("ROLE_ADMIN")
@@ -19,5 +23,15 @@ public class MenuController extends AbstractMenuController {
     @GetMapping("/{id}")
     public Menu getWithDishes(@PathVariable int id) {
         return super.getWithDishes(id);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Menu> createDish(@Valid @RequestBody MenuDTO menuDTO) {
+        Menu createdMenu = super.create(MenuUtil.asTo(menuDTO));
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentContextPath()
+                .path(REST_URL + "/{id}")
+                .buildAndExpand(createdMenu.getId()).toUri();
+        return ResponseEntity.created(uri).body(createdMenu);
     }
 }
