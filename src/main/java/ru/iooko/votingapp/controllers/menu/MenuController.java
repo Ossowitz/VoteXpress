096da63dac7364d98418a256a63deb4b71bb8446
Ilya Tikhomirov <76myxomor76@gmail.com>
2022-12-24 01:41:16 +0300

    Add get method in MenuController

--- src/main/java/ru/iooko/votingapp/controllers/menu/MenuController.java
+++ src/main/java/ru/iooko/votingapp/controllers/menu/MenuController.java
@@ -2,8 +2,11 @@ package ru.iooko.votingapp.controllers.menu;
 
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.iooko.votingapp.model.Menu;
 
 @RestController
 @Secured("ROLE_ADMIN")
@@ -12,4 +15,9 @@ import org.springframework.web.bind.annotation.RestController;
 public class MenuController extends AbstractMenuController {
 
     protected static final String REST_URL = "/api/menues";
+
+    @GetMapping("/{id}")
+    public Menu getWithDishes(@PathVariable int id) {
+        return super.getWithDishes(id);
+    }
 }
