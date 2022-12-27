30ba108f0c82effa3d5d7a9c1b37482beebb0d8f
Ilya Tikhomirov <76myxomor76@gmail.com>
2022-12-27 20:11:34 +0300

    Add get methods in RestaurantController

--- src/main/java/ru/iooko/votingapp/controllers/restaurant/RestaurantController.java
+++ src/main/java/ru/iooko/votingapp/controllers/restaurant/RestaurantController.java
@@ -2,8 +2,14 @@ package ru.iooko.votingapp.controllers.restaurant;
 
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.iooko.votingapp.dto.RestaurantDTO;
+import ru.iooko.votingapp.model.Restaurant;
+
+import java.util.List;
 
 @RestController
 @Secured("ROLE_ADMIN")
@@ -12,4 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class RestaurantController extends AbstractRestaurantController {
 
     protected static final String REST_URL = "/api/restaurants";
+
+    @GetMapping("/{id}")
+    public Restaurant get(@PathVariable int id) {
+        return super.get(id);
+    }
+
+    @GetMapping
+    @Secured({"ROLE_ADMIN", "ROLE_USER"})
+    public List<RestaurantDTO> getAllWithMenu() {
+        return super.getAllWithMenu();
+    }
 }
