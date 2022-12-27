296dc485f98742b4426cbef7a5510bc47ed8a94b
Ilya Tikhomirov <76myxomor76@gmail.com>
2022-12-27 20:47:23 +0300

    Add getWithDishes() and create methods

--- src/main/java/ru/iooko/votingapp/controllers/menu/AbstractMenuController.java
+++ src/main/java/ru/iooko/votingapp/controllers/menu/AbstractMenuController.java
@@ -2,7 +2,9 @@ package ru.iooko.votingapp.controllers.menu;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import ru.iooko.votingapp.model.Menu;
 import ru.iooko.votingapp.service.MenuService;
+import ru.iooko.votingapp.util.validation.ValidationUtil;
 
 @Slf4j
 public class AbstractMenuController {
@@ -10,4 +12,15 @@ public class AbstractMenuController {
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     private MenuService service;
+
+    public Menu getWithDishes(int id) {
+        log.info("get menu {}", id);
+        return service.getWithDishes(id);
+    }
+
+    public Menu create(Menu menu) {
+        log.info("create menu {}", menu);
+        ValidationUtil.checkNew(menu);
+        return service.create(menu);
+    }
 }
