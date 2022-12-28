7b3b633e9cf0e27232989d0e71b59175e8924efc
Ilya Tikhomirov <76myxomor76@gmail.com>
2022-12-28 19:50:29 +0300

    Add get methods in AbstractRestaurantController

--- src/main/java/ru/iooko/votingapp/controllers/restaurant/AbstractRestaurantController.java
+++ src/main/java/ru/iooko/votingapp/controllers/restaurant/AbstractRestaurantController.java
@@ -2,7 +2,12 @@ package ru.iooko.votingapp.controllers.restaurant;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import ru.iooko.votingapp.dto.RestaurantDTO;
+import ru.iooko.votingapp.model.Restaurant;
 import ru.iooko.votingapp.service.RestaurantService;
+import ru.iooko.votingapp.util.accessory.RestaurantUtil;
+
+import java.util.List;
 
 @Slf4j
 public class AbstractRestaurantController {
@@ -10,4 +15,14 @@ public class AbstractRestaurantController {
     @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     private RestaurantService service;
+
+    public Restaurant get(int id) {
+        log.info("get restaurant {}", id);
+        return service.get(id);
+    }
+
+    public List<RestaurantDTO> getAllWithMenu() {
+        log.info("getAllRestaurantsWithMenu");
+        return RestaurantUtil.getDTOs(service.getAllWithMenu());
+    }
 }
