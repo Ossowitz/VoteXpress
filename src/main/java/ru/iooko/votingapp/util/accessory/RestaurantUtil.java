37710954a0916e7b99e5b9f3f7ce5fefe930c799
Ilya Tikhomirov <76myxomor76@gmail.com>
2022-12-28 19:35:02 +0300

    Add asTo method RestaurantUtil

---
+++ src/main/java/ru/iooko/votingapp/util/accessory/RestaurantUtil.java
@@ -0,0 +1,35 @@
+package ru.iooko.votingapp.util.accessory;
+
+import lombok.experimental.UtilityClass;
+import ru.iooko.votingapp.dto.RestaurantDTO;
+import ru.iooko.votingapp.model.Menu;
+import ru.iooko.votingapp.model.Restaurant;
+import ru.iooko.votingapp.model.Votes;
+import ru.iooko.votingapp.security.SecurityUtil;
+
+import java.util.Set;
+
+@UtilityClass
+public class RestaurantUtil {
+
+    public static RestaurantDTO asTo(Restaurant restaurant) {
+        Menu menu = restaurant.getMenues()
+                .stream().findFirst()
+                .orElse(null);
+        if (menu == null || menu.getVotes() == null) {
+            return new RestaurantDTO(restaurant.getId(), restaurant.getName(), restaurant.getMenues(), 0, false);
+        }
+
+        Set<Votes> votes = menu.getVotes();
+        Integer size = votes.size();
+        boolean isVoted = votes.stream()
+                .anyMatch(
+                        it -> {
+                            int id = it.getUser().getId() == null ? 0 : it.getUser().getId();
+                            return id == SecurityUtil.authUserId();
+                        }
+                );
+        return new RestaurantDTO(restaurant.getId(), restaurant.getName(),
+                restaurant.getMenues(), size, isVoted);
+    }
+}
