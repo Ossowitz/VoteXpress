71e6fb3497340c5a6db37128d2248e6c99da11c3
Ilya Tikhomirov <76myxomor76@gmail.com>
2022-12-29 17:40:16 +0300

    Add get methods in AbstractDishController

--- src/main/java/ru/iooko/votingapp/web/controllers/dish/AbstractDishController.java
+++ src/main/java/ru/iooko/votingapp/web/controllers/dish/AbstractDishController.java
@@ -2,12 +2,25 @@ package ru.iooko.votingapp.web.controllers.dish;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import ru.iooko.votingapp.model.Dish;
 import ru.iooko.votingapp.service.DishService;
+
+import java.util.List;
 
 @Slf4j
 public class AbstractDishController {
 
-    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
+//    @SuppressWarnings("SpringJavaInjectionPointsAutowiringInspection")
     @Autowired
     private DishService service;
+
+    public Dish get(int id) {
+        log.info("get dish {}", id);
+        return service.get(id);
+    }
+
+    public List<Dish> getAll() {
+        log.info("getAll dish");
+        return service.getAll();
+    }
 }
