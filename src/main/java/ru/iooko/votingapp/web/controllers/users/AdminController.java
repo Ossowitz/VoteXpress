69c1fd45fdd717def1e3ffb0e7435268f02ed33c
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-01 06:57:57 +0300

    Add get() methods

--- src/main/java/ru/iooko/votingapp/web/controllers/users/AdminController.java
+++ src/main/java/ru/iooko/votingapp/web/controllers/users/AdminController.java
@@ -1,8 +1,13 @@
 package ru.iooko.votingapp.web.controllers.users;
 
 import org.springframework.security.access.annotation.Secured;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import ru.iooko.votingapp.model.Users;
+
+import java.util.List;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 
@@ -13,4 +18,14 @@ import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 public class AdminController extends AbstractUserController {
 
     protected static final String REST_URL = "/api/users";
+
+    @GetMapping
+    public List<Users> getAll() {
+        return super.getAll();
+    }
+
+    @GetMapping("/{id}")
+    public Users getByIdWithRoles(@PathVariable int id) {
+        return super.getByIdWithRoles(id);
+    }
 }
