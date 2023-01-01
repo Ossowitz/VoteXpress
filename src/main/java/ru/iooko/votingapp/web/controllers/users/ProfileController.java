c7d8b65f8f805fa7789d707fcb3f95be95dc07bc
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-01 06:30:12 +0300

    Add update() method in ProfileController

--- src/main/java/ru/iooko/votingapp/web/controllers/users/ProfileController.java
+++ src/main/java/ru/iooko/votingapp/web/controllers/users/ProfileController.java
@@ -1,9 +1,13 @@
 package ru.iooko.votingapp.web.controllers.users;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.security.access.annotation.Secured;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import ru.iooko.votingapp.dto.UsersDTO;
+import ru.iooko.votingapp.util.security.SecurityUtil;
+
+import javax.validation.Valid;
 
 @RestController
 @Secured("ROLE_USER")
@@ -13,4 +17,9 @@ public class ProfileController extends AbstractUserController {
 
     protected static final String REST_URL = "api/profile";
 
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void update(@Valid @RequestBody UsersDTO userDTO) {
+        super.update(userDTO, SecurityUtil.authUserId());
+    }
 }
