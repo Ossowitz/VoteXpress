96dfe866e4d5748523e9b545032ad75fb6879d0a
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-01 11:25:38 +0300

    Add Mapper configuration

---
+++ src/main/java/ru/iooko/votingapp/config/AppConfig.java
@@ -0,0 +1,23 @@
+package ru.iooko.votingapp.config;
+
+import com.fasterxml.jackson.databind.Module;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import ru.iooko.votingapp.util.JsonUtils;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    Module module() {
+        return new Hibernate5Module();
+    }
+
+    @Autowired
+    public void objectMapper(ObjectMapper mapper) {
+        JsonUtils.setMapper(mapper);
+    }
+}
