b87b0cff5bfa2060156f2282b66fc440a7d68d05
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-01 06:25:50 +0300

    Add get() and authUserId() methods in SecurityUtil

--- src/main/java/ru/iooko/votingapp/util/security/SecurityUtil.java
+++ src/main/java/ru/iooko/votingapp/util/security/SecurityUtil.java
@@ -4,9 +4,15 @@ import lombok.experimental.UtilityClass;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
+import java.util.Objects;
+
 @UtilityClass
 public class SecurityUtil {
 
+    /**
+     *  returns an AuthUser object that represents the currently authorized user.
+     *      If the user is not authorized, the method returns null
+     */
     public static AuthUser safeGet() {
         Authentication authentication = SecurityContextHolder.getContext()
                 .getAuthentication();
@@ -17,5 +23,19 @@ public class SecurityUtil {
         return (principal instanceof AuthUser) ? (AuthUser) principal : null;
     }
 
+    /**
+     * returns the currently logged in user, if one exists.
+     *      If the user is not authorized, the method throws a NullPointerException
+     */
+    public static AuthUser get() {
+        return Objects.requireNonNull(safeGet(), "No authorized user found");
+    }
 
+    /**
+     *  method returns the ID of the currently authorized user.
+     *      If the user is not authorized, the method throws a NullPointerException
+     */
+    public static int authUserId() {
+        return get().getUser().id();
+    }
 }
