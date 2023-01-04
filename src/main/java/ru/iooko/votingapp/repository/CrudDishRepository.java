e3daba37b2f7e663652c9ab408e1ff8283043e44
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-04 15:14:34 +0300

    Add CrudDishRepository

---
+++ src/main/java/ru/iooko/votingapp/repository/CrudDishRepository.java
@@ -0,0 +1,17 @@
+package ru.iooko.votingapp.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
+import ru.iooko.votingapp.model.Dish;
+
+@Transactional(readOnly = true)
+public interface CrudDishRepository extends JpaRepository<Dish, Integer> {
+
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM Dish d WHERE d.id=:id")
+    int delete(@Param("id") int id);
+}
