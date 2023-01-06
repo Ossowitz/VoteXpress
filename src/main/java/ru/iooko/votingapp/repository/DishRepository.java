4195ca3686019bb33267d45eb4417571c0e5ab80
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-06 20:25:51 +0300

    Add save() and delete() methods is DishRepository

--- src/main/java/ru/iooko/votingapp/repository/DishRepository.java
+++ src/main/java/ru/iooko/votingapp/repository/DishRepository.java
@@ -2,10 +2,22 @@ package ru.iooko.votingapp.repository;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+import ru.iooko.votingapp.model.Dish;
 
 @Repository
 @AllArgsConstructor
 public class DishRepository {
 
     private final CrudDishRepository repository;
+
+    @Transactional
+    public Dish save(Dish dish) {
+        return repository.save(dish);
+    }
+
+    @Transactional
+    public boolean delete(int id) {
+        return repository.delete(id) != 0;
+    }
 }
