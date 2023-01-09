f76858c500a8695b627ad60965f109940ca501ae
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-09 15:59:46 +0300

    Add find method in CrudRestaurantRepository

--- src/main/java/ru/iooko/votingapp/repository/CrudRestaurantRepository.java
+++ src/main/java/ru/iooko/votingapp/repository/CrudRestaurantRepository.java
@@ -1,11 +1,15 @@
 package ru.iooko.votingapp.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import ru.iooko.votingapp.model.Restaurant;
+
+import java.time.LocalDate;
+import java.util.List;
 
 @Transactional(readOnly = true)
 public interface CrudRestaurantRepository extends JpaRepository<Restaurant, Integer> {
@@ -14,4 +18,8 @@ public interface CrudRestaurantRepository extends JpaRepository<Restaurant, Inte
     @Modifying
     @Query("DELETE FROM Restaurant r WHERE r.id =: id")
     int delete(@Param("id") int id);
+
+    @EntityGraph(value = "allJoined", type = EntityGraph.EntityGraphType.LOAD)
+    @Query("SELECT r FROM Restaurant r LEFT JOIN r.menues m ON m.date = ?1 ORDER BY r.name")
+    List<Restaurant> findAllRestaurantWithMenuWithDishesWithVolesByDateIsNow(LocalDate today);
 }
