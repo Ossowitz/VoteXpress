db178d5ce2fdcb5f3f7e2a6afce376b6f8b484cc
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-10 23:08:16 +0300

    Add get-methods in RestaurantRepository

--- src/main/java/ru/iooko/votingapp/repository/RestaurantRepository.java
+++ src/main/java/ru/iooko/votingapp/repository/RestaurantRepository.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 import ru.iooko.votingapp.model.Restaurant;
 
+import java.time.LocalDate;
+import java.util.List;
+
 @Repository
 @AllArgsConstructor
 public class RestaurantRepository {
@@ -16,5 +19,13 @@ public class RestaurantRepository {
 
     public boolean delete(int id) {
         return repository.delete(id) != 0;
+    }
+
+    public Restaurant get(int id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    public List<Restaurant> getAllWithMenu() {
+        return repository.findAllRestaurantWithMenuWithDishesWithVotesByDateIsNow(LocalDate.now());
     }
 }
