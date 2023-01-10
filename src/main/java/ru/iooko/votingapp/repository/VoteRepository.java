e0765cb93aee4b040bd97f4203c86d7ffaafd777
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-10 23:27:41 +0300

    Add get-methods in VoteRepository

---
+++ src/main/java/ru/iooko/votingapp/repository/VoteRepository.java
@@ -0,0 +1,22 @@
+package ru.iooko.votingapp.repository;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+import ru.iooko.votingapp.model.Votes;
+
+import java.time.LocalDate;
+
+@Repository
+@AllArgsConstructor
+public class VoteRepository {
+
+    private final CrudVoteRepository repository;
+
+    public Votes getByIdWithMenuWithUser(int id) {
+        return repository.getByIdWithMenuWithUser(id).orElse(null);
+    }
+
+    public Votes getForUser(int userId) {
+        return repository.getWithMenuForUserOnToday(userId, LocalDate.now()).orElse(null);
+    }
+}
