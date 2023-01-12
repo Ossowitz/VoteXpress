d44d7cc9fb652ba492a59e669aa3b7bfbb41b0a2
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-12 12:25:46 +0300

    Add DateTimeUtil

---
+++ src/main/java/ru/iooko/votingapp/util/DateTimeUtil.java
@@ -0,0 +1,18 @@
+package ru.iooko.votingapp.util;
+
+import lombok.experimental.UtilityClass;
+
+import java.time.LocalTime;
+import java.time.format.DateTimeFormatter;
+
+@UtilityClass
+public class DateTimeUtil {
+
+    public static final String DATE_TIME_PATTERN = "HH-mm";
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter
+            .ofPattern(DATE_TIME_PATTERN);
+
+    public static String toString(LocalTime time) {
+        return time == null ? "" : time.format(DATE_TIME_FORMATTER);
+    }
+}
