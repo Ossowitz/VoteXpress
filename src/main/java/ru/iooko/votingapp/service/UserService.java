35e701aaff0d4d91f7e09b051f5e1560474998ac
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-13 22:33:50 +0300

    Add prepareSave method in UserService

---
+++ src/main/java/ru/iooko/votingapp/service/UserService.java
@@ -0,0 +1,30 @@
+package ru.iooko.votingapp.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import ru.iooko.votingapp.model.Users;
+import ru.iooko.votingapp.repository.UserRepository;
+import ru.iooko.votingapp.util.UserUtil;
+
+@Service("userService")
+@AllArgsConstructor
+public class UserService {
+
+    private final UserRepository repository;
+
+    // Restrict for modification
+    private boolean modificationAllowed = true;
+
+    public Users create(Users user) {
+        Assert.notNull(user, "user must not be null");
+
+        // todo: create method
+        return null;
+    }
+
+    // prepare user before save to db (including the encode)
+    private Users save(Users user) {
+        return repository.save(UserUtil.prepareToSave(user));
+    }
+}
