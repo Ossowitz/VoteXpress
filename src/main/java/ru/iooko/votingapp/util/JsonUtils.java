129f49f14c099c248b02504ca70e3fc70255d68f
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-13 21:34:35 +0300

    Add mapper class

---
+++ src/main/java/ru/iooko/votingapp/util/JsonUtils.java
@@ -0,0 +1,57 @@
+package ru.iooko.votingapp.util;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectReader;
+import lombok.experimental.UtilityClass;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+@UtilityClass
+public class JsonUtils {
+
+//    https://habr.com/ru/companies/otus/articles/687004/#:~:text=%D0%B8%20%D0%B8%D0%B7%20%D0%BD%D0%B5%D0%B3%D0%BE.-,ObjectMapper,-ObjectMapper%20%E2%80%94%20%D0%BD%D0%B0%D0%B8%D0%B1%D0%BE%D0%BB%D0%B5%D0%B5%20%D1%87%D0%B0%D1%81%D1%82%D0%BE
+    private static ObjectMapper mapper;
+
+    public static void setMapper(ObjectMapper mapper) {
+        JsonUtils.mapper = mapper;
+    }
+
+    public static <T> List<T> readValues(String json, Class<T> clazz) {
+        ObjectReader reader = mapper.readerFor(clazz);
+        try {
+            return reader.<T>readValues(json).readAll();
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Invalid read array from JSON:\n'" + json + "'", e);
+        }
+    }
+
+    public static <T> T readValue(String json, Class<T> clazz) {
+        try {
+            return mapper.readValue(json, clazz);
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Invalid read from JSON:\n'" + json + "'", e);
+        }
+    }
+
+    public static <T> String writeValue(T obj) {
+        try {
+            return mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new IllegalStateException("Invalid write to JSON:\n'" + obj + "'", e);
+        }
+    }
+
+    public static <T> String writeAdditionProps(T obj, String addName, Object addValue) {
+        return writeAdditionProps(obj, Map.of(addName, addValue));
+    }
+
+    public static <T> String writeAdditionProps(T obj, Map<String, Object> addProps) {
+        Map<String, Object> map = mapper.convertValue(obj, new TypeReference<>() {});
+        map.putAll(addProps);
+        return writeValue(map);
+    }
+}
