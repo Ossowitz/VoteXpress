860a88ebf0f6da6226107e7ff1f2a0ba23d53bb5
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-13 22:24:02 +0300

    Add User Utility class

---
+++ src/main/java/ru/iooko/votingapp/util/UserUtil.java
@@ -0,0 +1,36 @@
+package ru.iooko.votingapp.util;
+
+import lombok.experimental.UtilityClass;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import ru.iooko.votingapp.dto.UsersDTO;
+import ru.iooko.votingapp.model.Users;
+
+@UtilityClass
+public class UserUtil {
+
+    public static final PasswordEncoder PASSWORD_ENCODER = PasswordEncoderFactories.
+            createDelegatingPasswordEncoder();
+
+    /**
+     * @brief https://habr.com/ru/companies/otus/articles/552412/
+     * @// TODO: implement pattern builder
+     */
+    public static UsersDTO asTo(Users users) {
+        return new UsersDTO(users.getId(), users.getName(), users.getEmail(), users.getPassword());
+    }
+
+    public static Users updateFromTo(Users users, UsersDTO usersDto) {
+        users.setName(usersDto.getName());
+        users.setEmail(usersDto.getEmail().toLowerCase());
+        users.setPassword(usersDto.getPassword());
+        return users;
+    }
+
+    public static Users prepareToSave(Users users) {
+        users.getRoles().forEach(roles -> roles.setUser(users));
+        users.setPassword(PASSWORD_ENCODER.encode(users.getPassword()));
+        users.setEmail(users.getEmail().toLowerCase());
+        return users;
+    }
+}
