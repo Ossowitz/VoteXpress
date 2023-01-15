60c8e5a0ca65c067bd4726fcab8167b7bd89a4ae
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-15 14:49:06 +0300

    Create empty project

---
+++ src/main/java/ru/iooko/votingapp/VotingAppApplication.java
@@ -0,0 +1,13 @@
+package ru.iooko.votingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VotingAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VotingAppApplication.class, args);
+    }
+
+}
