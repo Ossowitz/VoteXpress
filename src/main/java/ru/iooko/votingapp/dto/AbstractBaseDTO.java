10fda6973e3dda8d2997cc081df3ddd46dc5f836
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-27 23:28:54 +0300

    Initial Commit

--- src/main/java/ru/iooko/votingapp/dto/AbstractBaseDTO.java
+++ src/main/java/ru/iooko/votingapp/dto/AbstractBaseDTO.java
@@ -2,9 +2,16 @@ package ru.iooko.votingapp.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.springframework.data.domain.Persistable;
 import org.springframework.util.Assert;
 
+@Data
+@AllArgsConstructor(access = AccessLevel.PROTECTED)
+@NoArgsConstructor
 public class AbstractBaseDTO implements Persistable<Integer> {
 
     @Schema(accessMode = Schema.AccessMode.READ_ONLY) // read-only and cannot be changed during save or update operations
