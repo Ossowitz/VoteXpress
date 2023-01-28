7a437a9886be2537667505976c4e03d02504016a
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-28 13:03:08 +0300

    Fix liquibase bugs

--- src/main/java/ru/iooko/votingapp/model/Menu.java
+++ src/main/java/ru/iooko/votingapp/model/Menu.java
@@ -1,6 +1,5 @@
 package ru.iooko.votingapp.model;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonManagedReference;
 import com.fasterxml.jackson.annotation.JsonProperty;
