ce1be1a10818dd623b58d5552737447987b13cda
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-29 23:26:12 +0300

    Add AbstractBaseDTO

---
+++ src/main/java/ru/iooko/votingapp/dto/AbstractBaseDTO.java
@@ -0,0 +1,29 @@
+package ru.iooko.votingapp.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import io.swagger.v3.oas.annotations.media.Schema;
+import org.springframework.data.domain.Persistable;
+import org.springframework.util.Assert;
+
+public class AbstractBaseDTO implements Persistable<Integer> {
+
+    @Schema(accessMode = Schema.AccessMode.READ_ONLY) // read-only and cannot be changed during save or update operations
+    protected Integer id;
+
+    @Override
+    public Integer getId() {
+        Assert.notNull(id, "Entity must have id");
+        return id;
+    }
+
+    @JsonIgnore
+    @Override
+    public boolean isNew() {
+        return id == null;
+    }
+
+    @Override
+    public String toString() {
+        return getClass().getSimpleName() + ":" + id;
+    }
+}
