96975aaf2bd26bd8c5d9b73f2a6e8336ffcf421d
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-01-30 18:51:04 +0300

    Add UsersDTO

--- src/main/java/ru/iooko/votingapp/dto/UsersDTO.java
+++ src/main/java/ru/iooko/votingapp/dto/UsersDTO.java
@@ -1,4 +1,30 @@
 package ru.iooko.votingapp.dto;
 
-public class UsersDTO {
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+import lombok.Value;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Value
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+public class UsersDTO extends AbstractNamedDTO {
+
+    @Email
+    @NotBlank
+    @Size(min = 2, max = 128)
+    String email;
+
+    @NotBlank
+    @Size(min = 8, max = 128)
+    String password;
+
+    public UsersDTO(Integer id, String name, String email, String password) {
+        super(id, name);
+        this.email = email;
+        this.password = password;
+    }
 }
