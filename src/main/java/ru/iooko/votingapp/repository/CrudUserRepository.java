c248ad8a43186015450e3077f18c7373916dfcfb
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-02-01 13:01:17 +0300

    Add method getByEmail() in CrudUserRepository

--- src/main/java/ru/iooko/votingapp/repository/CrudUserRepository.java
+++ src/main/java/ru/iooko/votingapp/repository/CrudUserRepository.java
@@ -7,10 +7,15 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 import ru.iooko.votingapp.model.Users;
 
+import java.util.Optional;
+
 public interface CrudUserRepository extends JpaRepository<Users, Integer> {
 
     @Transactional
     @Modifying
-    @Query("DELETE FROM Users u WHERE u.id=:id")
+    @Query("DELETE FROM Users u WHERE u.id =: id")
     int delete(@Param("id") int id);
+
+    @Query("FROM Users u LEFT JOIN FETCH u.roles r ")
+    Optional<Users> getByEmail(@Param("email") String email);
 }
