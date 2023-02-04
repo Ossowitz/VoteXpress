3377d4176c7883a3ac2e65701e8e447a6ba4e35d
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-02-04 18:04:43 +0300

    Add checkNotFoundWithId method in Validation class

---
+++ src/main/java/ru/iooko/votingapp/util/validation/ValidationUtil.java
@@ -0,0 +1,22 @@
+package ru.iooko.votingapp.util.validation;
+
+import lombok.experimental.UtilityClass;
+import ru.iooko.votingapp.exception.NotFoundException;
+
+import java.time.LocalTime;
+
+@UtilityClass
+public class ValidationUtil {
+
+    private static final LocalTime TIME_CONSTRAINT = LocalTime.of(11, 0, 0);
+
+    public static void checkNotFound(boolean found, String message) {
+        if (!found) {
+            throw new NotFoundException("Not found entity with " + message);
+        }
+    }
+
+    public static void checkNotFoundWithId(boolean found, int id) {
+        checkNotFound(found, "id=" + id);
+    }
+}
