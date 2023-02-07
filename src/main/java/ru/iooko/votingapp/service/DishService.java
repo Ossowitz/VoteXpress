dd9ce9f83c29ce280c339b4907c6bb68526d9916
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-02-07 23:42:20 +0300

    Add create method in DishService

--- src/main/java/ru/iooko/votingapp/service/DishService.java
+++ src/main/java/ru/iooko/votingapp/service/DishService.java
@@ -2,11 +2,19 @@ package ru.iooko.votingapp.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
+import ru.iooko.votingapp.model.Dish;
 import ru.iooko.votingapp.repository.DishRepository;
+import ru.iooko.votingapp.util.validation.ValidationUtil;
 
 @Service("dishService")
 @AllArgsConstructor
 public class DishService {
 
     private final DishRepository repository;
+
+    public Dish create(Dish dish) {
+        Assert.notNull(dish, "dish must not be null");
+        return ValidationUtil.checkNotFoundWithId(repository.save(dish), dish.getId());
+    }
 }
