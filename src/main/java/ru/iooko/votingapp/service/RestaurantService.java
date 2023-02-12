247cbd673357000a4fa4fddbbf755db7d8474066
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-02-12 19:28:16 +0300

    Add update and get methods in RestaurantService

--- src/main/java/ru/iooko/votingapp/service/RestaurantService.java
+++ src/main/java/ru/iooko/votingapp/service/RestaurantService.java
@@ -2,10 +2,13 @@ package ru.iooko.votingapp.service;
 
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
-import org.springframework.util.Assert;
 import ru.iooko.votingapp.model.Restaurant;
 import ru.iooko.votingapp.repository.RestaurantRepository;
-import ru.iooko.votingapp.util.validation.ValidationUtil;
+
+import java.util.List;
+
+import static org.springframework.util.Assert.notNull;
+import static ru.iooko.votingapp.util.validation.ValidationUtil.checkNotFoundWithId;
 
 @Service("restaurantService")
 @AllArgsConstructor
@@ -14,11 +17,24 @@ public class RestaurantService {
     private final RestaurantRepository repository;
 
     public Restaurant create(Restaurant restaurant) {
-        Assert.notNull(restaurant, "restaurant must not be null");
+        notNull(restaurant, "restaurant must not be null");
         return repository.save(restaurant);
     }
 
     public void delete(int id) {
-        ValidationUtil.checkNotFoundWithId(repository.delete(id), id);
+        checkNotFoundWithId(repository.delete(id), id);
+    }
+
+    public Restaurant update(Restaurant restaurant) {
+        notNull(restaurant, "restaurant must not be null");
+        return checkNotFoundWithId(repository.save(restaurant), restaurant.getId());
+    }
+
+    public Restaurant get(int id) {
+        return checkNotFoundWithId(repository.get(id), id);
+    }
+
+    public List<Restaurant> getAllWithMenu() {
+        return repository.getAllWithMenu();
     }
 }
