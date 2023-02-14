cc00332bda5566151249cf6ba2ffbf5bd7b6eb09
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-02-14 21:52:04 +0300

    Add create method in MenuService and some Refactoring

--- src/main/java/ru/iooko/votingapp/service/MenuService.java
+++ src/main/java/ru/iooko/votingapp/service/MenuService.java
@@ -4,7 +4,10 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import ru.iooko.votingapp.model.Menu;
 import ru.iooko.votingapp.repository.MenuRepository;
-import ru.iooko.votingapp.util.validation.ValidationUtil;
+
+import static java.time.LocalDate.now;
+import static org.springframework.util.Assert.notNull;
+import static ru.iooko.votingapp.util.validation.ValidationUtil.checkNotFoundWithId;
 
 @Service
 @AllArgsConstructor
@@ -13,10 +16,16 @@ public class MenuService {
     private final MenuRepository repository;
 
     public Menu get(int id) {
-        return ValidationUtil.checkNotFoundWithId(repository.get(id), id);
+        return checkNotFoundWithId(repository.get(id), id);
     }
 
     public Menu getWithDishes(int id) {
-        return ValidationUtil.checkNotFoundWithId(repository.getWithDishes(id), id);
+        return checkNotFoundWithId(repository.getWithDishes(id), id);
+    }
+
+    public Menu create(Menu menu) {
+        notNull(menu, "Menu must not be null");
+        menu.setDate(now());
+        return repository.save(menu);
     }
 }
