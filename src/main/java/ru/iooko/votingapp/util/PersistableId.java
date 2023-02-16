f2e762f99a1c2f0ccf7ec8fe082b7a81550195a9
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-02-16 15:26:10 +0300

    Add PersistableId

---
+++ src/main/java/ru/iooko/votingapp/util/PersistableId.java
@@ -0,0 +1,21 @@
+package ru.iooko.votingapp.util;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.data.domain.Persistable;
+import org.springframework.util.Assert;
+
+public interface PersistableId extends Persistable<Integer> {
+    @Override
+    Integer getId();
+
+    @Override
+    @JsonIgnore
+    default boolean isNew() {
+        return getId() == null;
+    }
+
+    default int id() {
+        Assert.notNull(getId(), "Entity must has valid id");
+        return getId();
+    }
+}
