6a14800952ef919f6121148f4e19d1ecb42627f3
Ilya Tikhomirov <76myxomor76@gmail.com>
2023-02-16 16:06:00 +0300

    Add create and update method in AbstractUserController

--- src/main/java/ru/iooko/votingapp/web/controllers/users/AbstractUserController.java
+++ src/main/java/ru/iooko/votingapp/web/controllers/users/AbstractUserController.java
@@ -2,8 +2,10 @@ package ru.iooko.votingapp.web.controllers.users;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import ru.iooko.votingapp.dto.UsersDTO;
 import ru.iooko.votingapp.model.Users;
 import ru.iooko.votingapp.service.UserService;
+import ru.iooko.votingapp.util.validation.ValidationUtil;
 
 import java.util.List;
 
@@ -26,5 +28,23 @@ public class AbstractUserController {
     public Users getByIdWithRoles(int id) {
         log.info("getByIdWithRoles {}", id);
         return service.getByIdWithRoles(id);
+    }
+
+    public Users create(Users user) {
+        log.info("create {}", user);
+        ValidationUtil.checkNew(user);
+        return service.create(user);
+    }
+
+    public void update(Users user, int id) {
+        log.info("update {} with id={}", user, id);
+        ValidationUtil.assureIdConsistent(user, id);
+        service.update(user);
+    }
+
+    public void update(UsersDTO userDTO, int id) {
+        log.info("update {} with id={}", userDTO, id);
+        ValidationUtil.assureIdConsistent(userDTO, id);
+        service.update(userDTO);
     }
 }
